58b401ecb8f0104e1893bc90f6fde1064c02fdfe
Leandro Antonio Romero <leandroaromero@yahoo.com.ar>
2020-06-11 00:16:21 -0300

    Updates

---
+++ precache-manifest.1e1038335a7fa2f5860fc20e9d107b47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3095aa2ded92e08a90d7c5323d884511",
+    "url": "/prueba/index.html"
+  },
+  {
+    "revision": "79ed81b32cfabf5212c7",
+    "url": "/prueba/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/prueba/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prueba/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79ed81b32cfabf5212c7",
+    "url": "/prueba/static/js/main.6b48f2a7.chunk.js"
+  },
+  {
+    "revision": "ed5b6e940357a94b7e7d",
+    "url": "/prueba/static/js/runtime-main.afde5860.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prueba/static/media/logo.5d5d9eef.svg"
+  }
+]);
